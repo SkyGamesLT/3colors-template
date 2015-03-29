62fee2ff75fbfec9aa356f5f92eb3ea9568d7318
Raimondas <github@mekdrop.name>
2015-03-29 17:42:25 +0300

    Imported xoops green template

---
+++ xoops/green/js/startup.js
@@ -0,0 +1,3 @@
+function Body_OnLoad() {
+	FP_preloadImgs(/*url*/'images/rollover/images/rollover_01.gif', /*url*/'images/rollover/images/rollover_02.gif', /*url*/'images/rollover/images/rollover_03.gif', /*url*/'images/rollover/images/rollover_04.gif', /*url*/'images/rollover/images/rollover_05.gif', /*url*/'images/rollover/images/rollover_06.gif');
+}
